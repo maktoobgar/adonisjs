49d5bf5e89b74732a22e005d49ed9f2692039c6d
Mahmood Heidari <mahmoodh1378@gmail.com>
2021-06-04 21:48:45 +0430

    ✅ user and posts database migrations added

---
+++ database/migrations/1622805056082_users.ts
@@ -0,0 +1,20 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Users extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').unsigned().primary()
+      table.string('username', 64).notNullable().unique()
+      table.string('email', 64).notNullable().unique()
+      table.string('password', 128).notNullable()
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
---
+++ database/migrations/1622805383587_posts.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Posts extends BaseSchema {
+  protected tableName = 'posts'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').unsigned().primary()
+      table.string('title', 64).notNullable()
+      table.text('context').notNullable()
+      table.integer('user_id').unsigned()
+        .notNullable().references('users.id')
+        .onDelete('CASCADE')
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
