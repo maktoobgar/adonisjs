11050617b7f5c02b3f10a1797624c9c40c9040cc
Mahmood Heidari <mahmoodh1378@gmail.com>
2021-06-06 16:12:00 +0430

    🔧 based on new middlewares, extra lines removed

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -12,28 +12,24 @@ export default class PostsController {
       // if (!Post.$getRelation('user').booted) { Post.$getRelation('user').boot() }   // relation needs to boot to use down functions and set relationships
       // Post.$getRelation('user').setRelated(post, auth.user)            // creates the relationship just on user field and not userId
       // Post.$getRelation('user').hydrateForPersistance(post, auth.user) // sets the value for userId in post.userId field and not on user
-      await post.load('user')   // loads user variable by userId, doesn't matter if you call this after or before post.save() function
       await post.save()
-      response.status(201).json(post)
+      await post.load('user')   // loads user variable by userId, doesn't matter if you call this after or before post.save() function but better to call it after
+      response.created(post)
     }
   }
   public async delete({ response, params }: HttpContextContract) {
-    const post = await Post.findOrFail(params.id)
-    await post.delete()
+    await params.post.delete()
     response.noContent()
   }
   public async show({ params, response }: HttpContextContract) {
-    const post = await Post.find(params.id)
-    if (post) {
-      await post?.load('user')
-      response.status(200).json(post)
+    if (params.post) {
+      response.ok(params.post)
       return
     }
-    response.status(400).json({ 'message': 'requested post not found' })
   }
   public async update({ params, response, request }: HttpContextContract) {
     await request.validate(UpdatePostValidator)
-    const post = await Post.findOrFail(params.id)
+    const post = params.post
     const { title, context } = request.only(['title', 'context'])
     post.title = Boolean(title) ? title : (request.method() === 'PATCH' ? post.title : "")
     post.context = Boolean(context) ? context : (request.method() === 'PATCH' ? post.context : "")
